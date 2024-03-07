79603a68d995f98cb91f5c426dc2aaeb828625ba
Nantes07 <hnantes@foursys.com.br>
2024-03-07 15:07:25 -0300

    transferenciaImpl3 feita

---
+++ src/main/java/br/com/cdb/dao/impl3/TranferenciaDaoImpl3.java
@@ -0,0 +1,40 @@
+package br.com.cdb.dao.impl3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.cdb.dao.TransferenciaDao;
+import br.com.cdb.dao.repository.TransferenciaRepository;
+import br.com.cdb.entity.Transferencia;
+
+public class TranferenciaDaoImpl3 implements TransferenciaDao {
+
+	@Autowired
+	private TransferenciaRepository transferenciaRepository;
+
+	@Override
+	public void salvar(Transferencia transferencia) {
+		transferenciaRepository.save(transferencia);
+
+	}
+
+	@Override
+	public List<Transferencia> historico(int numero) {
+		List<Transferencia> historico = new ArrayList<>();
+		for (Transferencia t : transferenciaRepository.findAll()) {
+			if (t.getPessoa1() == numero || t.getPessoa2() == numero) {
+				historico.add(t);
+			}
+		}
+		return historico;
+	}
+
+	@Override
+	public List<Transferencia> transferencia() {
+
+		return transferenciaRepository.findAll();
+	}
+
+}
